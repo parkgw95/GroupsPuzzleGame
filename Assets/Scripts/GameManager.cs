9cfdb94164f667cbeafeaffef1d5b986e09b8c9f
Gwanmo Park <8743001+parkgw95@users.noreply.github.com>
2020-07-20 03:55:30 +0900

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite circleSprite;
+    public Sprite hexagonSprite;
+    public Sprite diamondSprite;
+
+    public Color blue;
+    public Color red;
+    public Color green;
+
+    private List<PanelController> selectedPanels;
+
+    void Start()
+    {
+        selectedPanels = new List<PanelController>();
+    }
+
+    public void selectPanel(PanelController panel)
+    {
+        if (selectedPanels.Contains(panel)) return;
+
+        selectedPanels.Add(panel);
+        if (selectedPanels.Count == 3)
+        {
+            Debug.Log(checkGroup(selectedPanels));
+            if (checkGroup(selectedPanels))
+            {
+                refill();
+            } else
+            {
+                refresh();
+            }
+        }
+    }
+
+    public void deselectPanel(PanelController panel)
+    {
+        if (!selectedPanels.Contains(panel)) return;
+
+        selectedPanels.Remove(panel);
+    }
+
+    public void refill()
+    {
+        List<PanelController> tmp = new List<PanelController>(selectedPanels);
+        foreach (PanelController panel in tmp)
+        {
+            panel.randomize();
+            panel.deselect();
+        }
+    }
+
+    public void refresh()
+    {
+        List<PanelController> tmp = new List<PanelController>(selectedPanels);
+        foreach (PanelController panel in tmp)
+        {
+            panel.deselect();
+        }
+    }
+
+    bool checkGroup(List<PanelController> panels)
+    {
+        if (panels.Count != 3) return false;
+
+        return (checkGroupColor(panels) && checkGroupShape(panels));
+    }
+
+    bool checkGroupShape(List<PanelController> panels)
+    {
+        if (panels[0].shape == panels[1].shape && panels[1].shape == panels[2].shape) return true;
+        if (panels[0].shape != panels[1].shape && panels[1].shape != panels[2].shape && panels[0].shape != panels[2].shape) return true;
+        return false;
+    }
+
+    bool checkGroupColor(List<PanelController> panels)
+    {
+        if (panels[0].color == panels[1].color && panels[1].color == panels[2].color) return true;
+        if (panels[0].color != panels[1].color && panels[1].color != panels[2].color && panels[0].color != panels[2].color) return true;
+        return false;
+    }
+}
+
+public enum PShape { circle, hexagon, diamond };
+public enum PColor { blue, red, green };
---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    public int id;
+    public bool selected { get; private set; }
+    public MeshRenderer backplate;
+    public Material backplateDefault;
+    public Material backplateSelected;
+    public SpriteRenderer sprite;
+
+    private GameManager gm;
+
+    public PShape shape { get; private set; }
+    public PColor color { get; private set; }
+
+    void Start()
+    {
+        selected = false;
+        gm = GameObject.Find("Background Board").GetComponent<GameManager>();
+        randomize();
+    }
+
+    void OnMouseDown()
+    {
+        if (selected)
+        {
+            deselect();
+        } else
+        {
+            select();
+        }
+    }
+
+    public void select()
+    {
+        if (selected) return;
+        selected = true;
+        backplate.material = backplateSelected;
+        gm.selectPanel(this);
+    }
+
+    public void deselect()
+    {
+        if (!selected) return;
+        selected = false;
+        backplate.material = backplateDefault;
+        gm.deselectPanel(this);
+    }
+
+    public void randomize()
+    {
+        changeShape((PShape)UnityEngine.Random.Range(0, 3));
+        changeColor((PColor)UnityEngine.Random.Range(0, 3));
+    }
+
+    void changeShape(PShape newShape)
+    {
+        shape = newShape;
+        if (newShape == PShape.circle)
+        {
+            sprite.sprite = gm.circleSprite;
+        } else if (newShape == PShape.hexagon)
+        {
+            sprite.sprite = gm.hexagonSprite;
+        } else if (newShape == PShape.diamond)
+        {
+            sprite.sprite = gm.diamondSprite;
+        }
+    }
+
+    void changeColor(PColor newColor)
+    {
+        color = newColor;
+        if (newColor == PColor.blue)
+        {
+            sprite.color = gm.blue;
+        } else if (newColor == PColor.red)
+        {
+            sprite.color = gm.red;
+        } else if (newColor == PColor.green)
+        {
+            sprite.color = gm.green;
+        }
+    }
+}
